8bec76beb9218d03e7a4e9775de1883f0f7c7320
Mathias Millet <mathiasmillet@gmail.com>
2015-11-20 23:35:09 +0100

    un fichier org pour mettre un peu les choses au clair

--- molecule.ml
+++ molecule.ml
@@ -10,7 +10,18 @@ type acid =
 
 type molecule = acid list;;
 
+(* On commence par extraire les labels des noeuds et des transitions, 
+qu'on met dans deux listes qui vont bien.
+On associe en même temps les arcs avec leurs noeuds :
+une liste d'arcs par noeuds.
 
+Mais que fait on si plusieurs noeuds ou transitions partagent le 
+même label ??? :(
+
+
+
+
+ *)
 let rec get_nodes_and_transitions_string mol nodesL transL = 
   match mol with
   | Node (s,_) :: mol' ->       get_nodes_and_transitions_string mol' (s::nodesL) transL
@@ -19,6 +30,9 @@ let rec get_nodes_and_transitions_string mol nodesL transL =
   | _ :: mol' ->            get_nodes_and_transitions_string mol' nodesL transL
 ;;
 
+
+(* retourne la premier position de l'item recherché 
+(ou -1 s'il n'est pas là *)
 let list_position e l =
   let rec aux l n =  
     match l with
@@ -28,8 +42,13 @@ let list_position e l =
   in aux l 0
 ;;
   
+(* Il faut ensuite construire pour chaque transition 
+les tableaux des arcs entrants et sortants.
+On construit d'abord des listes.
+*) 
 let rec build_transitions trans_array mol = 
-  
+  match mol with
+  | InputLink (s,t) -> 
 
 
 
