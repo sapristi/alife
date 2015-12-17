4db1f3e88f7d04bb9a9b5dc25fae2198f243448e
Mathias Millet <mathiasmillet@gmail.com>
2015-12-17 23:41:14 +0100

    erreur

--- _build/src/bacterie/reactions.ml
+++
@@ -1,8 +0,0 @@
-let reaction_constant = 0.001
-
-
-(* à améliorer ? *)
-let react (n : int) (m : int) : bool = 
-  let r = Random.float 1. in
-  r < (reaction_constant *. (float_of_int n) *. (float_of_int m))
-    
