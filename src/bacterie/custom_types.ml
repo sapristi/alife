308efee2b38136ccdb02e2a94adc10877a24f83b
sapristi <mathiasmillet@gmail.com>
2017-07-28 20:12:56 +0200

    started reorganising code

--- src/bacterie/custom_types.ml
+++ src/bacterie/custom_types.ml
@@ -8,41 +8,70 @@ open Molecule
 
 
 (* * type definitions *)
-(* on va d'abord essayer de définir les types des arcs correctement pour pouvoir travailler sur les transitions plus tranquillement *)
 
+(* ** description générale *)
+(*    :in-progress: *)
+(*    Implémentation des types différents acides. Voilà en gros l'organisation : *)
+(*   + place : aucune action *)
+(*   + transition : agit sur la molécule contenue dans un token *)
+(*     - regular : rien de particulier *)
+(*     - split : coupe une molécule en 2 (seulement transition_input) *)
+(*     - bind : insère une molécule dans une autre (seulement transition_output) *)
+(*   + extension : autres ? *)
+(*     - handle : poignée pour attraper cette molécule *)
+(*       problème : il faudrait pouvoir attrapper la molécule à n'importe quel acide ? ou alors on attrappe la poignée directement et pas la place associée *)
+(*     - catch : permet d'attraper une molécule. *)
+(*       Est ce qu'il y a une condition d'activation, par exemple un token vide (qui contiendrait ensuite la molécule) ? *)
+(*     - release : lache la molécule attachée *)
+(*     - move : déplace le point de contact de la molécule *)
+(*     - send : envoie un message *)
+(*     - receive : receives a message *)
+
+(*   Questions : est-ce qu'on met l'action move sur les liens ou en extension ? dans les liens c'est plus cohérent, mais dans les extensions ça permet d'en mettre plusiers à la suite. Par contre, à quel moment est-ce qu'on déclenche l'effet de bord ? En recevant le token d'une transition.  Mais du coup pour l'action release, il faudrait aussi la mettre sur les places, puisqu'on agit aussi à l'extérieur du token. Du coup pour l'instant on va mettre à la fois move et release dans les extensions, avec un système pour appliquer les effets des extensions quand on reçoit un token. *)
+
+(* ** place *)
+(* Aucun effet de bord pour l'instant *)
 type place_type = 
-  | Initial_place
   | Regular_place
-  | Handle_place of string
-  | Catch_place of string
-  | Receive_place of string
-  | Release_place
-  | Send_place of string
-  | Displace_mol_place of bool
       [@@deriving show, yojson]
       
-      
+
+(* ** transition_input *)
 type transition_input_type = 
   | Regular_ilink
   | Split_ilink
       [@@deriving show, yojson]
 
+(* ** transition_output *)
 type transition_output_type = 
   | Regular_olink
   | Bind_olink
-  | Mol_output_olink
+      [@@deriving show, yojson]
+
+(* ** extension *)
+type extension_type =
+  | Handle_ext of string
+  | Catch_ext of string
+  | Receive_ext of string
+  | Release_ext
+  | Move_ext of bool
+  | Send_ext of string
+  | Displace_mol_ext of bool
       [@@deriving show, yojson]
 
 (* * MoleculeManager instantiation *)
 (* defines the MolTypes struct using the defined types, and then instantiates the MoleculeManager *)
 
-module MolTypes = struct 
-    type nodeType = place_type
-      [@@deriving show, yojson]
-    type transitionInputType = transition_input_type
-      [@@deriving show, yojson]
-    type transitionOutputType = transition_output_type
-      [@@deriving show, yojson]
+module AcidTypes = struct 
+  type placeType = place_type
+                     [@@deriving show, yojson]
+  type transitionInputType = transition_input_type
+                               [@@deriving show, yojson]
+  type transitionOutputType = transition_output_type
+                                [@@deriving show, yojson]
+  type extensionType = extension_type
+                          [@@deriving show, yojson]
+                              
 end
 
-module MyMolecule = MakeMoleculeManager(MolTypes);;
+module MyMolecule = MakeMoleculeManager(AcidTypes);;
--- src/bacterie/molecule.ml
+++ src/bacterie/molecule.ml
@@ -18,23 +18,25 @@ open Misc_library
 (*   Les types sont implémentés dans le fichier custom_types.ml *)
 
    
-module type MOLECULE_TYPES = 
-sig 
-  type nodeType
-      [@@deriving show, yojson]
-  type transitionInputType
-      [@@deriving show, yojson]
-  type transitionOutputType
-    [@@deriving show, yojson]
-
-end;;
+module type ACID_TYPES = 
+  sig 
+    type placeType
+           [@@deriving show, yojson]
+    type extensionType
+           [@@deriving show, yojson]
+    type transitionOutputType
+           [@@deriving show, yojson]
+    type transitionInputType
+           [@@deriving show, yojson]
+       
+  end;;
 
 
 (* * MoleculeManager functor *)
-(*   Functor that creates the molecule type and associated functions, given implementations of nodeType, transitionInputType and transitionOutputType *)
+(*   Functor that creates the molecule type and associated functions, given implementations of placeType, transitionInputType and transitionOutputType *)
 
 module MakeMoleculeManager = 
-  functor (MolTypes : MOLECULE_TYPES) -> 
+  functor (AcidTypes : ACID_TYPES) -> 
 struct 
   
 (* ** type definitions *)
@@ -46,13 +48,13 @@ struct
 (*  + a piece of information : ???? *)
   
   type acid = 
-    | Node of MolTypes.nodeType
-    | TransitionInput of string * MolTypes.transitionInputType
-    | TransitionOutput of string * MolTypes.transitionOutputType
-    | Information of string
+    | Node of AcidTypes.placeType
+    | TransitionInput of string * AcidTypes.transitionInputType
+    | TransitionOutput of string * AcidTypes.transitionOutputType
+    | Extension of AcidTypes.extensionType
                        [@@deriving show, yojson]
                    
-                                      
+                   
   type molecule = acid list
                        [@@deriving show, yojson]
                 
@@ -64,132 +66,156 @@ struct
                     [@@deriving show]
 
 
-(* ***  transition structure type definition *)
+(* *** transition structure type definition *)
 (* Transition Input and Output can combine when they share the same identifier, to form a transition. For now, we only put all incomming and outgoing edges into the structure, whose function will be determined later. *)
 
-
+                
   type transition_structure = 
     string * 
-      (int * MolTypes.transitionInputType ) list * 
-        (int * MolTypes.transitionOutputType) list
-                                            [@@deriving show]
+      (int * AcidTypes.transitionInputType ) list * 
+        (int * AcidTypes.transitionOutputType) list
+                                               [@@deriving show]
 
-					 
+(* *** place extensions definition *)
+
+  type place_extensions =
+    AcidTypes.extensionType list
+    
 (* ** functions definitions *)
 (* *** build_transitions function *)
 
 (* This function builds the transitions associated with a molecule. This will be used when folding the molecule to get functional form. *)
-
-
 (* Il faut ensuite construire pour chaque transition les tableaux des arcs entrants et sortants. On fait tout en un seul passage sur la molecule, en espérant qu'il n'y ait pas trop de transitions. *)
 (* On retourne une liste contenant les items (transID, transitionInputs, transitionOutputs) *)
 
   let build_transitions (mol : molecule) :
         transition_structure list = 
-  
-    (* insère un arc entrant dans la bonne transition 
-     de la liste des transitions *)
-    let rec insert_new_input 
-      (nodeN :   int) 
-      (transID : string) 
-      (data :    MolTypes.transitionInputType) 
-      (transL :  transition_structure list) : 
-      
-      transition_structure list =
     
-    match transL with
-    | (t, input, output) :: transL' -> 
-       if transID = t 
-       then (t,  (nodeN, data) :: input, output) :: transL'
-       else (t, input, output) :: (insert_new_input nodeN transID data transL')
-    | [] -> [transID, [nodeN, data], []]
+    (* insère un arc entrant dans la bonne transition 
+       de la liste des transitions *)
+    let rec insert_new_input 
+              (nodeN :   int) 
+              (transID : string) 
+              (data :    AcidTypes.transitionInputType) 
+              (transL :  transition_structure list) : 
+              
+              transition_structure list =
       
-
- 
-  (* insère un arc sortant dans la bonne transition 
-     de la liste des transitions *)
-  and insert_new_output 
-      (nodeN :   int) 
-      (transID : string)
-      (data :    MolTypes.transitionOutputType) 
-      (transL :  transition_structure list) :
-
-      transition_structure list =  
-
-    match transL with
-    | (t, input, output) :: transL' -> 
-       if transID = t 
-       then (t,  input, (nodeN, data) ::  output) :: transL'
-       else (t, input, output) :: (insert_new_output nodeN transID data transL')
-    | [] -> [transID, [], [nodeN, data]]
+      match transL with
+      | (t, input, output) :: transL' -> 
+         if transID = t 
+         then (t,  (nodeN, data) :: input, output) :: transL'
+         else (t, input, output) :: (insert_new_input nodeN transID data transL')
+      | [] -> [transID, [nodeN, data], []]
+            
+            
+    (* insère un arc sortant dans la bonne transition 
+       de la liste des transitions *)
+    and insert_new_output 
+          (nodeN :   int) 
+          (transID : string)
+          (data :    AcidTypes.transitionOutputType) 
+          (transL :  transition_structure list) :
+          
+          transition_structure list =  
+      
+      match transL with
+      | (t, input, output) :: transL' -> 
+         if transID = t 
+         then (t,  input, (nodeN, data) ::  output) :: transL'
+         else (t, input, output) :: (insert_new_output nodeN transID data transL')
+      | [] -> [transID, [], [nodeN, data]]
+            
+    in 
+    let rec aux 
+              (mol :    molecule)
+              (nodeN :  int) 
+              (transL : transition_structure list) :
+              
+              transition_structure list = 
+      
+      match mol with
+      | Node _ :: mol' -> aux mol' (nodeN + 1) transL
+      | TransitionInput (s,d) :: mol' -> aux mol' nodeN (insert_new_input nodeN s d transL)
+      | TransitionOutput (s,d) :: mol' -> aux mol' nodeN (insert_new_output nodeN s d transL)
+      | Extension _ :: mol' -> aux mol' nodeN transL
+      | [] -> transL
        
-  in 
-  let rec aux 
-      (mol :    molecule)
-      (nodeN :  int) 
-      (transL : transition_structure list) :
-
-      transition_structure list = 
- 
-    match mol with
-    | Node _ :: mol' -> aux mol' (nodeN + 1) transL
-    | TransitionInput (s,d) :: mol' -> aux mol' nodeN (insert_new_input nodeN s d transL)
-    | TransitionOutput (s,d) :: mol' -> aux mol' nodeN (insert_new_output nodeN s d transL)
-    | Information _ :: mol' -> aux mol' nodeN transL
-    | [] -> transL
-       
-  in 
-  aux mol (-1) []
+    in 
+    aux mol (-1) []
     
 (* *** build_nodes_list function *)
 (*     Extrait la liste des noeuds, de la molécule, dans l'ordre rencontré   *)
 
-let rec build_nodes_list (mol : molecule) : MolTypes.nodeType list = 
-  match mol with
-  | Node d :: mol' -> d :: (build_nodes_list mol')
-  | _ :: mol' -> build_nodes_list mol'
-  | [] -> []
+  let rec build_nodes_list (mol : molecule) : AcidTypes.placeType list = 
+    match mol with
+    | Node d :: mol' -> d :: (build_nodes_list mol')
+    | _ :: mol' -> build_nodes_list mol'
+    | [] -> []
 
+(* *** build_nodes_list_with_exts function *)
+(* Construit la liste des nœuds avec les extensions associéeNote : l'ordre des liste sera dans l'ordre inverse de l'ordre initial de la molécule *)
+
+  let build_nodes_list_with_exts (mol : molecule) :
+        (AcidTypes.placeType * (AcidTypes.extensionType list)) list =
+    
+    let rec aux mol res = 
+      match mol with
+      | Node n :: mol' -> aux mol' ((n, []) ::res)
+      | Extension e :: mol' ->
+         begin
+           match res with
+           | [] -> aux mol' res
+           | (n, ext_l) :: res' ->
+              aux mol' ((n, e :: ext_l) :: res)
+         end
+      | _ :: mol' -> aux mol' res
+      | [] -> res
+    in
+    aux mol []
+    
 (* ** the MoleculeHolder module *)
 (* Module used to manage a molecule attached at some position to another molecule : defines functions to change the position of attach, cut the molecule, and insert another molecule at position           *)
   module MoleculeHolder =
-  struct 
-    type t = molecule * int
-      [@@deriving show, yojson]
-          
-    let is_empty (h : t) : bool = 
-      let m,p = h in m = []
-    let empty = ([],0)
-      
-    let make (mol : molecule) : t = mol,0
+    struct 
+      type t = molecule * int
+                            [@@deriving show, yojson]
+             
+      let is_empty (h : t) : bool = 
+        let m,p = h in m = []
+                     
+      let empty = ([],0)
+                
+      let make (mol : molecule) : t = mol,0
+                                    
+      let make_at_pos (mol : molecule) (pos : int) : t = mol,pos
+                                                       
+      let get_molecule (h : t) : molecule =
+        let m,_ = h in m
+                     
+      let move_forward (h : t) : t =
+        let m,p = h in  m, p+1
+                      
+      let move_backward (h : t) : t =
+        let m,p = h in  m, p-1
+                      
+      let get_mol_length (h : t) : int = 
+        let m,p = h in  List.length m
+                      
+      let get_postion (h : t) : int = 
+        let m,p = h in p
+                     
+      let cut (h : t) : t*t =
+        let mol, pos = h in 
+        let m1, m2 = Misc_library.cut_list mol pos in
+        (m1,pos), (m2, 0)
         
-    let make_at_pos (mol : molecule) (pos : int) : t = mol,pos
-        
-    let get_molecule (h : t) : molecule =
-      let m,_ = h in m
-         
-    let move_forward (h : t) : t =
-      let m,p = h in  m, p+1
-        
-    let move_backward (h : t) : t =
-      let m,p = h in  m, p-1
-        
-    let get_mol_length (h : t) : int = 
-      let m,p = h in  List.length m
-         
-    let get_postion (h : t) : int = 
-      let m,p = h in p
-         
-    let cut (h : t) : t*t =
-      let mol, pos = h in 
-      let m1, m2 = Misc_library.cut_list mol pos in
-      (m1,pos), (m2, 0)
-           
-    let insert (h1 : t) (h2 : t) : t =
-      match h1, h2 with
-      |  (m1, p1), (m2, p2) ->
+      let insert (h1 : t) (h2 : t) : t =
+        match h1, h2 with
+        |  (m1, p1), (m2, p2) ->
          (Misc_library.insert m1 p1 m2), 0
-  end
-
-              
+    end
+    
+    
 end;;
+  
