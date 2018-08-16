cf7c99029b4b7e13b1c5a19bc733e807f6881928
sapristi <megabigfoutoir@gmail.com>
2018-08-16 15:05:53 +0200

    mockup test implemented, we need to improve reporter to keep on

--- src/tests/reacs1.ml
+++ src/tests/reacs1.ml
@@ -1,10 +1,24 @@
 open Bacterie_libs;;
+open OUnit2;;
 
-let bact_json = Yojson.Safe.from_file "bact_states/simple_bind.json" in
-    match Bacterie.of_yojson bact_json  with
-    | Ok bact -> "ok"
-    | Error s -> s
-    
+let b = ref (Bacterie.make_empty ());;
+     
+
+let test1 test_ctx =
+  assert_equal (
+      
+      let bact_json = Yojson.Safe.from_file "bact_states/simple_bind.json" in
+      match Bacterie.of_yojson bact_json  with
+      | Ok bact ->
+         b := bact;
+         "ok"
+      | Error s -> s) "ok"
+
+
+
+
+
+               
 let env :Environment.t = {transition_rate = 1.;
                           grab_rate = 1.;
                           break_rate = 0.;
@@ -12,6 +26,11 @@ let env :Environment.t = {transition_rate = 1.;
 
 
 
-let bact = Bacterie.make_empty ~env:env;
+let bact = Bacterie.make_empty ~env:env;;
 
-print_endline (
+let suite =
+  "suite">:::
+    ["test1">::test1]
+
+let () =
+  run_test_tt_main suite;;
