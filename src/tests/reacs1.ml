00877f68973bf2b3b2b2a532e548e6ded4e4a980
sapristi <megabigfoutoir@gmail.com>
2019-03-07 22:23:24 +0100

    grab_release test

--- src/tests/reacs1.ml
+++ src/tests/reacs1.ml
@@ -72,12 +72,38 @@ and simple_break test_ctx =
     ~printer:Bacterie.show_bact_sig 
     expected_result result
 
+and simple_grab_release test_ctx =
+  
+  let sandbox = Sandbox.of_yojson
+                  ( Yojson.Safe.from_file "bact_states/simple_grab_release.json" ) 
+  in 
+  Bacterie.next_reaction !(sandbox.bact);
+  Bacterie.next_reaction !(sandbox.bact);
+
+
+  
+  let result = Bacterie.to_sig !(sandbox.bact)
+               |> Bacterie.canonical_bact_sig 
+             
+  and expected_result : Bacterie.bact_sig =
+    Bacterie.canonical_bact_sig
+      {active_mols = [
+         {mol="AAABAAADDFABAFAFDDFAAACAAADDFABB";qtt=1}];
+       inert_mols = [{mol="A"; qtt=1; ambient=false};]}
+    
+  in
+  
+  assert_equal
+    ~printer:Bacterie.show_bact_sig 
+    expected_result result
   
 let suite =
   "suite">:::
     ["simple bind">::simple_bind;
      "simple split">::simple_split;
-    "simple break">::simple_break;]
+     "simple break">::simple_break;
+     "simple_grab_release">::simple_grab_release;
+    ]
   
 let () =
   run_test_tt_main suite;;
